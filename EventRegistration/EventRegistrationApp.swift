8016386661b0974bfaf05b0d14f7df5fcaa9eb5b
Karthick <mail4karthickr@gmail.com>
2025-08-13 05:35:26 +0530

    Initial Commit

---
+++ EventRegistration/EventRegistrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventRegistrationApp.swift
+//  EventRegistration
+//
+//  Created by Karthick Ramasamy on 13/08/25.
+//
+
+import SwiftUI
+
+@main
+struct EventRegistrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
